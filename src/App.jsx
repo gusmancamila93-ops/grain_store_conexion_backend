7d0c226dcba3f1a26ca3440736c124e0462193d0
Camila Guzman <gusmancamila93@gmail.com>
2026-06-23 16:00:49 -0500

    Configuración inicial del proyecto con Vite y Tailwind funcionando

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="p-8">
+      <Button>Grain Store</Button>
+    </div>
+  );
+}
+
+export default App;
